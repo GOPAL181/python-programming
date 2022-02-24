4b8f2ca7ea2754e17a959fa92b563f05a3961b4d
Gopal Sharma <92174457+GOPAL181@users.noreply.github.com>
2022-02-24 19:31:29 +0530

    Add files via upload
    WAP to find distance between two points
    hint  (((a-b)**2)+((c-d)**2)**0.5)

---
+++ distance.py
@@ -0,0 +1,7 @@
+a=int(input("enter the value"))
+b=int(input("enter the value"))
+c=int(input("enter the value"))
+d=int(input("enter the value"))
+n=((a-b)**2)+((c-d)**2)
+dis=n**0.5
+print("distance",dis)
