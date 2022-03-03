dcec9bc402bf85386c7c5ec13f87995f241dac55
Gopal Sharma <92174457+GOPAL181@users.noreply.github.com>
2022-03-03 18:29:17 +0530

    Add files via upload
    write a program which take two value from user and find LCM and its HCF.

---
+++ lcm_hcf.py
@@ -0,0 +1,12 @@
+a=int(input("enter the value"))
+b=int(input("enter the value"))
+if(a>b):
+    min=b
+else:
+    min=a
+for i in range(1,min):
+    if(a%i==0 and b%i==0):
+        hcf=i
+print("hcf=",hcf)
+lcm=a*b//hcf
+print("lcm=",lcm)
